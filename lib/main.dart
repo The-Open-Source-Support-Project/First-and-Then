0073ca801bd503ca33e0a58b3600395093613709
Evan Lynch <e.lynch303@gmail.com>
2020-08-19 14:28:39 -0400

    update for android tv/ small style updaes / still need a new image picker for non mobile platforms

--- lib/main.dart
+++ lib/main.dart
@@ -15,32 +15,36 @@ class FirstAndThen extends StatelessWidget {
     SystemChrome.setEnabledSystemUIOverlays([]);
     final SettingsModel _settingModel = SettingsModel();
     return new ScopedModel<SettingsModel>(
-      model: _settingModel,
-      child: MaterialApp(
-        title: 'First And Then',
-        debugShowCheckedModeBanner:false,
-        theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        brightness: Brightness.dark,
-        primaryColor: Colors.blue,
-        accentColor: Colors.pinkAccent,
-        fontFamily: 'Roboto',
-        textTheme: TextTheme(
-          body1: TextStyle(fontSize: 18.0),
-          title: TextStyle(fontSize: 33.0),
-        ),
-      ),
-      home: MyHomePage(title: 'First And Then'),
-      ),
-    );
+        model: _settingModel,
+        child: Shortcuts(
+          shortcuts: <LogicalKeySet, Intent>{
+            LogicalKeySet(LogicalKeyboardKey.select): ActivateIntent(),
+          },
+          child: MaterialApp(
+            title: 'First And Then',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              // This is the theme of your application.
+              //
+              // Try running your application with "flutter run". You'll see the
+              // application has a blue toolbar. Then, without quitting the app, try
+              // changing the primarySwatch below to Colors.green and then invoke
+              // "hot reload" (press "r" in the console where you ran "flutter run",
+              // or simply save your changes to "hot reload" in a Flutter IDE).
+              // Notice that the counter didn't reset back to zero; the application
+              // is not restarted.
+              brightness: Brightness.dark,
+              primaryColor: Colors.blue,
+              accentColor: Colors.pinkAccent,
+              fontFamily: 'Roboto',
+              textTheme: TextTheme(
+                body1: TextStyle(fontSize: 18.0),
+                title: TextStyle(fontSize: 33.0),
+              ),
+            ),
+            home: MyHomePage(title: 'First And Then'),
+          ),
+        ));
   }
 }
 
@@ -74,72 +78,72 @@ class _MyHomePageState extends State<MyHomePage> {
     return ScopedModelDescendant<SettingsModel>(
         builder: (BuildContext context, Widget child, SettingsModel model) {
       return Scaffold(
-          body: model.timer == true ? boardWithTimer(model) : firstThenBoard(model.locked),
-          floatingActionButton: FloatingActionButton(
-            onPressed: (){
-              Navigator.push(context,
-                MaterialPageRoute<Settings>(
-                    builder: (BuildContext context) => Settings()),
-              );
-            },
-            tooltip: 'Settings',
-            child: Icon(Icons.settings),
-            backgroundColor: Colors.pinkAccent,
-          ),
+        body: model.timer == true
+            ? boardWithTimer(model)
+            : firstThenBoard(model.locked),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute<Settings>(
+                  builder: (BuildContext context) => Settings()),
+            );
+          },
+          tooltip: 'Settings',
+          child: Icon(Icons.settings),
+          backgroundColor: Colors.pinkAccent,
+          focusColor: Colors.pink,
+        ),
       );
     });
   }
 
-  Widget boardWithTimer(model){
+  Widget boardWithTimer(model) {
     var startTime;
-    void _restartTimer() async{
+    void _restartTimer() async {
       startTime = DateTime.now();
       setState(() {});
     }
+
     return Material(
         child: Column(
-          children: <Widget>[
-            Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.all(8.0),
-                    margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: FlipClock.countdown(
-                      startTime: startTime,
-                      duration: Duration(minutes: model.durationMin, seconds: model.durationSec),
-                      digitColor: Colors.white,
-                      backgroundColor: Colors.black,
-                      digitSize: 48.0,
-                      borderRadius: const BorderRadius.all(Radius.circular(3.0)),
-                      onDone: () => print('Done'),//TODO make this show Toast message
-                    ),
-                  ),
-                  IconButton(onPressed: _restartTimer, icon: Icon(Icons.refresh))
-                ]
+      children: <Widget>[
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: FlipClock.countdown(
+              startTime: startTime,
+              duration: Duration(
+                  minutes: model.durationMin, seconds: model.durationSec),
+              digitColor: Colors.white,
+              backgroundColor: Colors.black,
+              digitSize: 48.0,
+              borderRadius: const BorderRadius.all(Radius.circular(3.0)),
+              onDone: () => print('Done'), //TODO make this show Toast message
             ),
-            Expanded(
-              child:firstThenBoard(model.locked),
-            )
-          ],
+          ),
+          IconButton(onPressed: _restartTimer, icon: Icon(Icons.refresh))
+        ]),
+        Expanded(
+          child: firstThenBoard(model.locked),
         )
-
-    );
+      ],
+    ));
   }
 
-  Widget firstThenBoard(locked){
-    return OrientationBuilder(
-        builder: (context, orientation) {
-          return GridView.count(
-            padding: const EdgeInsets.all(20.0),
-            // Create a grid with 1 columns in portrait mode, or 2 columns in
-            // landscape mode.
-            crossAxisCount: orientation == Orientation.portrait ? 1 : 2,
-            children: <Widget>[
-              new ImageCard(title: "First", locked: locked),
-              new ImageCard(title: "Then", locked: locked)
-            ],
-          );
-        });
+  Widget firstThenBoard(locked) {
+    return OrientationBuilder(builder: (context, orientation) {
+      return GridView.count(
+        padding: const EdgeInsets.all(20.0),
+        // Create a grid with 1 columns in portrait mode, or 2 columns in
+        // landscape mode.
+        crossAxisCount: orientation == Orientation.portrait ? 1 : 2,
+        children: <Widget>[
+          new ImageCard(title: "First", locked: locked),
+          new ImageCard(title: "Then", locked: locked)
+        ],
+      );
+    });
   }
 }
--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -4,45 +4,44 @@ import 'package:scoped_model/scoped_model.dart';
 import 'package:toggle_switch/toggle_switch.dart';
 
 class Settings extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return ScopedModelDescendant<SettingsModel>(
         builder: (BuildContext context, Widget child, SettingsModel model) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Settings'),
-            ),
-            body: Center(
-              child: ListView(
-                children: <Widget>[
-                  Card(
-                    child: ListTile(
-                      title: Text('Screen Lock State:'),
-                      trailing: ToggleSwitch(
-                          minWidth: 90.0,
-                          cornerRadius: 20,
-                          initialLabelIndex: model.locked == false ? 0 : 1,
-                          activeBgColor: Colors.pinkAccent,
-                          activeTextColor: Colors.white,
-                          inactiveBgColor: Colors.grey,
-                          inactiveTextColor: Colors.white,
-                          labels: ['Unlocked', 'Locked'],
-                          icons: [Icons.lock_open, Icons.lock],
-                          onToggle: (index) {
-                            model.lockScreen(index);
-                          }),
-                    ),
-                  ),
-                  _timerSettingsCard(model),
-                ],
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Settings'),
+        ),
+        body: Center(
+          child: ListView(
+            children: <Widget>[
+              Card(
+                child: ListTile(
+                  title: Text('Screen Lock State:'),
+                  trailing: ToggleSwitch(
+                      minWidth: 120.0,
+                      cornerRadius: 8.0,
+                      initialLabelIndex: model.locked == false ? 0 : 1,
+                      activeBgColor: Colors.pinkAccent,
+                      activeTextColor: Colors.white,
+                      inactiveBgColor: Colors.grey,
+                      inactiveTextColor: Colors.white,
+                      labels: ['Unlocked', 'Locked'],
+                      icons: [Icons.lock_open, Icons.lock],
+                      onToggle: (index) {
+                        model.lockScreen(index);
+                      }),
+                ),
               ),
-            ),
-          );
-        });
+              _timerSettingsCard(model),
+            ],
+          ),
+        ),
+      );
+    });
   }
 
-  Widget _timerSettingsCard(model){
+  Widget _timerSettingsCard(model) {
     final _timeDurationForm = GlobalKey<FormState>();
     TextEditingController minutesController = TextEditingController();
     TextEditingController secondsController = TextEditingController();
@@ -51,96 +50,93 @@ class Settings extends StatelessWidget {
     return Form(
       key: _timeDurationForm,
       child: Card(
-        child:Container(
-          child: Column(
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      padding: const EdgeInsets.all(8.0),
-                      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Text(
-                        'Timer Settings',
-                        textAlign: TextAlign.center
-                      ),
+          child: Container(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text('Timer Settings', textAlign: TextAlign.center),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: ListTile(
+                    title: Text('Toggle Timer'),
+                    subtitle:
+                        Text('This will show a timer above the first item'),
+                    trailing: Switch(
+                      onChanged: model.toggleTimer,
+                      value: model.timer,
                     ),
                   ),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child:ListTile(
-                      title: Text('Toggle Timer'),
-                      subtitle: Text('This will show a timer above the first item'),
-                      trailing: Switch(
-                        onChanged: model.toggleTimer,
-                        value: model.timer,
-                      ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                        'Set Timer Duration (Timer Counts Down for the set duration)',
+                        textAlign: TextAlign.left),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: TextField(
+                      controller: minutesController,
+                      decoration: InputDecoration(labelText: "Minutes"),
+                      keyboardType: TextInputType.number,
+                      onChanged: (m) => minutesController.text = m,
                     ),
                   ),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      padding: const EdgeInsets.all(8.0),
-                      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Text(
-                          'Set Timer Duration (Timer Counts Down for the set duration)',
-                          textAlign: TextAlign.left
-                      ),
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: TextField(
+                      controller: secondsController,
+                      decoration: InputDecoration(labelText: "Seconds"),
+                      keyboardType: TextInputType.number,
+                      onChanged: (s) => secondsController.text = s,
                     ),
                   ),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child:Container(
-                      padding: const EdgeInsets.all(8.0),
-                      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: TextField(
-                        controller: minutesController,
-                        decoration: InputDecoration(labelText: "Minutes"),
-                        keyboardType: TextInputType.number,
-                        onChanged: (m) => minutesController.text = m,
-                      ),
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: RaisedButton(
+                      color: Colors.pinkAccent,
+                      onPressed: () {
+                        model.updateTimerDuration(
+                            minutesController.text, secondsController.text);
+                      },
+                      child: Text('Set Timer'),
                     ),
                   ),
-                  Expanded(
-                    child:Container(
-                      padding: const EdgeInsets.all(8.0),
-                      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: TextField(
-                        controller: secondsController,
-                        decoration: InputDecoration(labelText: "Seconds"),
-                        keyboardType: TextInputType.number,
-                        onChanged: (s) => secondsController.text = s,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child:Container(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: RaisedButton(
-                        color: Colors.pinkAccent,
-                        onPressed: () {
-                            model.updateTimerDuration(minutesController.text, secondsController.text);
-                        },
-                        child: Text('Set Timer'),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        )
-      ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
